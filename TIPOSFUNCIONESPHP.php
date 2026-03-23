11401518d97bc7adbb291831aca55773bb501644
Josué David Culajay Altán <davidaltan2540@gmail.com>
2026-03-23 13:42:16 -0600

    Funciones del 1- 6

--- TIPOSFUNCIONESPHP.php
+++ TIPOSFUNCIONESPHP.php
@@ -41,4 +41,44 @@ $anio  = substr($texto, 10, 4);  // Desde posición 10, toma 4 caracteres
 echo "5. substr — País: $pais | Año: $anio \n";
 
 
+
+//6. FUNCIÓN PARA BUSCAR POSICIÓN DE UN TEXTO
+// Devuelve la posición donde aparece por primera vez un texto asimismo retorna False si no lo encuentra.
+$texto = "PHP es genial y PHP es rápido";
+$pos = strpos($texto, "PHP");   // Primera ocurrencia → posición 0
+echo "6. strpos: posición $pos \n";
+
+
+//7. FUNCIÓN PARA ELIMINAR ESPACIONES AL INICIO Y AL FINAL 
+// Esta función elimina los espacios en blanco al inicio y al final de una cadena
+//ahora ltrim() elimina los espacios al inicio y rtrim() elimina los espacios a la derecha
+
+
+$sucio = "   Hola Mundo   ";
+$limpio = trim($sucio);
+echo "7. trim: '$limpio' \n"; // 'Hola Mundo'
+
+
+//8. FUNCIÓN REPETIR UNA CADENA
+//ESTA FUNCIÓN REPETIE UNA CADENA N VECES CONSECUTIVAS
+$linea = str_repeat("=", 30);
+echo "8. str_repeat: $linea \n"; // ==============================
+
+//9. FUNCIÓN PRIMERA LETRA EN MÁYUSCULA
+// Convierte la primera letra de una cadena en mayuscula
+
+$nombre = "josué culajay";
+echo "9. ucfirst: " . ucfirst($nombre) . " \n"; // Josué culajay
+
+//10. Dividir cadena en array
+// esta función divdide una cadena en partes utilizando un delimitador y devuelve un array con las partes resultantes.
+
+
+$csv    = "nombre,edad,ciudad";
+$campos = explode(",", $csv);
+// Resultado: ["nombre", "edad", "ciudad"]
+echo "10. explode: ";
+print_r($campos);
+
+
 ?>
