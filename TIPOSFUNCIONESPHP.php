8d68abc684d9894bb7ad22692c2cdee3e7dbedc6
Josué David Culajay Altán <davidaltan2540@gmail.com>
2026-03-23 13:24:40 -0600

    funcion longitud cadena

---
+++ TIPOSFUNCIONESPHP.php
@@ -0,0 +1,13 @@
+<?php
+
+//Tipos de funciones en PHP
+
+//1. FUNCION para verificar la longitud o cuantos caracteres tiene  una cadena
+
+$cadena = "Hola Mundo";
+$longitud = strlen($cadena);
+echo "1. strlen: $longitud \n"; // Resultado: 10
+
+
+
+?>
